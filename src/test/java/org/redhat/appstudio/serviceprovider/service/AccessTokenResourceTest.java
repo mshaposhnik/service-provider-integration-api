0cdc2682132b1c7ca240c70fc026f217bdfc636d
Max Shaposhnik <mshaposh@redhat.com>
2021-09-24 11:42:48 +0300

    Restore license
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

--- src/test/java/org/redhat/appstudio/serviceprovider/service/AccessTokenResourceTest.java
+++ src/test/java/org/redhat/appstudio/serviceprovider/service/AccessTokenResourceTest.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.redhat.appstudio.serviceprovider.service;
 
 import static io.restassured.RestAssured.given;
